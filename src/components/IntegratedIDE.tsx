69bfcc171e823d40ce4529333c7bbe9bfe1662d3
Sinehan Ezhilmuthu <ezsinehan@gmail.com>
2024-03-27 09:51:23 -0700

    setup boilerplate for integratedIDE

---
+++ src/components/IntegratedIDE.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+interface IntegratedIDEProps {}
+
+const IntegratedIDE: React.FC<IntegratedIDEProps> = () => {
+  return <div>hello samarth and sinehan</div>;
+};
+
+export default IntegratedIDE;
