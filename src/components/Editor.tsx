9a1ea299f1126eafd2910ad0b1c225444edb37af
sshramesh <samarth.shilpar@gmail.com>
2024-03-29 16:21:08 -0700

    jgothome(start)

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,12 +1,16 @@
 "use client";
 
-import React from "react";
+import React, {useState} from "react";
 import { Editor as MonacoEditor } from "@monaco-editor/react";
 
 const Editor: React.FC = () => {
+  const [value, setValue] = useState('')
   return (
     <div>
-      <MonacoEditor height="90vh" language="python" value="//somecomment" />
+      <MonacoEditor height="75vh" language="python" value={value} 
+      onChange={
+          (value, event) => setValue(value)
+      }/>
     </div>
   );
 };
