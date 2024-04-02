f09fe2c932aaeb40f131249c01c83af003a59656
Sinehan Ezhilmuthu <ezsinehan@gmail.com>
2024-04-01 23:38:30 -0700

    set up language selector, rdy to work on code runner

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,23 +1,32 @@
 "use client";
 
 import React, { useState, useRef } from "react";
-import { Monaco, Editor as MonacoEditor } from "@monaco-editor/react";
+import { Editor as MonacoEditor } from "@monaco-editor/react";
+import LanguageSelector from "./LanguageSelector";
+import { CODE_SNIPPETS } from "@/constants";
 
 const Editor: React.FC = () => {
   const editorRef = useRef<any | null>(null);
   const [value, setValue] = useState<string>("");
+  const [language, setLanguage] = useState<string>("javascript");
 
   const onMount = (editor: any | null) => {
     editorRef.current = editor;
     editor?.focus();
   };
 
+  const onSelect = (language: string) => {
+    setLanguage(language);
+    setValue(CODE_SNIPPETS[language]);
+  };
+
   return (
     <div>
+      <LanguageSelector language={language} onSelect={onSelect} />
       <MonacoEditor
         height="75vh"
-        defaultLanguage="javascript"
-        defaultValue="//some comment"
+        language={language}
+        defaultValue={CODE_SNIPPETS[language]}
         onMount={onMount}
         value={value}
         onChange={(newValue = "") => {
---
+++ src/components/LanguageSelector.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import React, { useState } from "react";
+import { LANGUAGE_VERSIONS } from "@/constants";
+
+interface LanguageSelectorProps {
+  language: string;
+  onSelect: (language: string) => void;
+}
+
+const Languages = Object.entries(LANGUAGE_VERSIONS);
+
+const LanguageSelector: React.FC<LanguageSelectorProps> = ({
+  language,
+  onSelect,
+}) => {
+  const handleLanguageChange = (event: any) => {
+    onSelect(event.target.value);
+  };
+
+  return (
+    <div>
+      <h1>Language Selector</h1>
+      <select value={language} onChange={handleLanguageChange}>
+        <option value="">Select a Language</option>
+        {Languages.map(([language, version]) => (
+          <option key={language} value={language}>
+            {language} - {version}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+export default LanguageSelector;
