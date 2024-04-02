2ad977719f079ee6aa86b482de80ca67ef7b1788
Sinehan Ezhilmuthu <ezsinehan@gmail.com>
2024-04-01 22:59:14 -0700

    editor basic setup, tsx types jus used any lol

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,16 +1,30 @@
 "use client";
 
-import React, {useState} from "react";
-import { Editor as MonacoEditor } from "@monaco-editor/react";
+import React, { useState, useRef } from "react";
+import { Monaco, Editor as MonacoEditor } from "@monaco-editor/react";
 
 const Editor: React.FC = () => {
-  const [value, setValue] = useState('')
+  const editorRef = useRef<any | null>(null);
+  const [value, setValue] = useState<string>("");
+
+  const onMount = (editor: any | null) => {
+    editorRef.current = editor;
+    editor?.focus();
+  };
+
   return (
     <div>
-      <MonacoEditor height="75vh" language="python" value={value} 
-      onChange={
-          (value, event) => setValue(value)
-      }/>
+      <MonacoEditor
+        height="75vh"
+        defaultLanguage="javascript"
+        defaultValue="//some comment"
+        onMount={onMount}
+        value={value}
+        onChange={(newValue = "") => {
+          setValue(newValue);
+          console.log(value);
+        }}
+      />
     </div>
   );
 };
